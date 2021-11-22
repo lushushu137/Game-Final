9c03f9da149d2e241dd4b09b4911d2f65fb2582c
lushu <lushu100fen@163.com>
2021-11-22 20:21:08 +0800

    dialogue iterator

--- Dialogue.js
+++ Dialogue.js
@@ -1,83 +1,133 @@
-let order = 0;
 
-export default function renderDialogue(){
-    if (order === 0) {
-        document.getElementById("dialogue_guest").style.opacity = 1;
+function* iterator (list) {
+    for (let item of list) {
+        yield item;
     }
-    if (order === 1) {
-        document.getElementById("dialogue_player").style.opacity = 1;
-    }
-    let currentDialogue = dialogueList.find((obj) => obj.order === order);
-    let currentCharacter = currentDialogue.character;
-    let currentContent = currentDialogue.content;
-    document.getElementById(`dialogue_${currentCharacter}`).innerText = currentContent;
-    order++;
 }
 
-export const dialogueList = [
-    {
-        character: 'guest',
-        content: 'Harder, please.',
-        order: 0
-    },
-    {
-        character: 'player',
-        content: 'Ok.',
-        order: 1
-    },
-    {
-        character: 'player',
-        content: 'You got pimples on your back.',
-        order: 2
-    },
-    {
-        character: 'guest',
-        content: 'Yeah... I hate that.',
-        order: 3
-    },
-    {
-        character: 'player',
-        content: 'Well, at least it proves you are young enough.',
-        order: 4
-    },
-    {
-        character: 'guest',
-        content: 'Right, but what is so good about being young? They are not only on my back, they are everywhere. My face, my neck, even my leg.',
-        order: 5
-    },
-    {
-        character: 'player',
-        content: 'Does that bother you?',
-        order: 6
-    },
-    {
-        character: 'guest',
-        content: 'Very much. Youth is curse.',
-        order: 7
-    },
-    {
-        character: 'player',
-        content: 'Interesting.',
-        order: 8
-    },
-    {
-        character: 'guest',
-        content: 'There is much you don\'t know. ',
-        order: 9
-    },
-    {
-        character: 'player',
-        content: 'Finished.',
-        order: 10
-    },
-    {
-        character: 'guest',
-        content: 'Bye. ',
-        order: 11
-    },
-    {
-        character: 'player',
-        content: 'Bye. ',
-        order: 12
-    },
-]
+export class Dialogue {
+    dialogueList;
+    beginIterator;
+    nextIterator;
+    endIterator;
+    finish = false;
+    constructor(dialogueList = dialogueMap){
+        this.dialogueList = dialogueList;
+        this.beginIterator = iterator(dialogueList.begin);
+        this.nextIterator = iterator(dialogueList.next);
+        this.iterator = iterator(dialogueList.end);
+    }
+    begin(){
+        let getBegin = setInterval(()=> {
+            let curr = this.beginIterator.next();
+            if (!curr.done) {
+                this.render(curr.value.character, curr.value.content);
+            } else {
+                clearInterval(getBegin);
+            }
+        }, 1000)
+    }
+
+    next(){
+        let curr = this.nextIterator.next();
+        if (!curr.done) {
+            this.render(curr.value.character, curr.value.content);
+        } else {
+            this.finish = true;
+            this.end();
+        }
+    }
+
+    end(){
+        let getEnd = setInterval(()=> {
+            let curr = this.iterator.next();
+            if (!curr.done) {
+                this.render(curr.value.character, curr.value.content);
+            } else {
+                clearInterval(getEnd);
+            }
+        }, 1000)
+    }
+
+    render(character, content){
+        document.getElementById(`dialogue_${character}`).style.opacity = 1;
+        document.getElementById(`dialogue_${character}`).innerText = content;
+    }
+}
+
+
+export const dialogueMap = {
+    begin: [
+        {
+            character: 'guest',
+            content: 'Harder, please.',
+            order: 0
+        },
+        {
+            character: 'player',
+            content: 'Ok.',
+            order: 1
+        },
+    ],
+    next: [
+        {
+            character: 'player',
+            content: 'You got pimples on your back.',
+            order: 2
+        },
+        {
+            character: 'guest',
+            content: 'Yeah... I hate that.',
+            order: 3
+        },
+        {
+            character: 'player',
+            content: 'Well, at least it proves you are young enough.',
+            order: 4
+        },
+        {
+            character: 'guest',
+            content: 'Right, but what is so good about being young? They are not only on my back, they are everywhere. My face, my neck, even my leg.',
+            order: 5
+        },
+        {
+            character: 'player',
+            content: 'Does that bother you?',
+            order: 6
+        },
+        {
+            character: 'guest',
+            content: 'Very much. Youth is curse.',
+            order: 7
+        },
+        {
+            character: 'player',
+            content: 'Interesting.',
+            order: 8
+        },
+        {
+            character: 'guest',
+            content: 'There is much you don\'t know. ',
+            order: 9
+        },
+    ],
+    end: [
+   
+   
+        {
+            character: 'player',
+            content: 'Finished.',
+            order: 10
+        },
+        {
+            character: 'guest',
+            content: 'Bye. ',
+            order: 11
+        },
+        {
+            character: 'player',
+            content: 'Bye. ',
+            order: 12
+        },
+    ],
+}
--- Game.js
+++ Game.js
@@ -1,12 +1,12 @@
 import { Woman } from "./Woman.js";
 import { KeyboardToAreaList } from "./KeyboardToAreaList.js";
-import renderDialogue from './Dialogue.js';
+import {Dialogue} from './Dialogue.js';
 
 // let scrubber = new Scrubber();
 let woman = new Woman();
 let inputList = [];
 let start = 0;
-
+let dialogue = new Dialogue();
 let dirtState = {
     "00": true,
     "01":true,
@@ -27,10 +27,15 @@ let clearArea = (inputList) => {
         if (isCovered) {
             let coordinate = `${KeyboardToAreaList[i].row}${KeyboardToAreaList[i].col}`;
             if (dirtState[coordinate]) {
+                // remove dirt view
                 document.getElementById(coordinate).style.opacity = 0;
+                // animate charactor
                 document.getElementById("player").style.backgroundImage = 'url(/assets/player.gif)';
                 dirtState[coordinate] = false;
-                renderDialogue();
+                dialogue.next();
+                if (dialogue.finish) {
+                    document.getElementById("player").style.backgroundImage = 'url(/assets/player.png)';
+                }
             }
         }
     }
@@ -51,5 +56,6 @@ let checkScroll = () => {
 }
 
 checkScroll();
+dialogue.begin();
 woman.render();
 

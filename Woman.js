afa2c88b53ba7b8a1ccd8ec0b2561ea1406fba5c
lushu <lushu100fen@163.com>
2021-12-03 10:26:12 +0800

    img path

--- Woman.js
+++ Woman.js
@@ -1,51 +1,55 @@
-import { aliceDialogueMap, susanDialogueMap, carolineDialogueMap} from './allDialogue.js';
+import {
+  aliceDialogueMap,
+  susanDialogueMap,
+  carolineDialogueMap,
+} from "./allDialogue.js";
 
 export class Woman {
-    womanIterator = iterator(womanList);
-    currWoman;
-    emmm;
-    constructor(){
-        this.currWoman = this.womanIterator.next().value;
-        this.render();
-        this.emmm = new Audio("./assets/audio/Human Female Mmm 01.wav")
-        this.emmm.volume = 0.2
-        this.emmm.play()
+  womanIterator = iterator(womanList);
+  currWoman;
+  emmm;
+  constructor() {
+    this.currWoman = this.womanIterator.next().value;
+    this.render();
+    this.emmm = new Audio("./assets/audio/Human Female Mmm 01.wav");
+    this.emmm.volume = 0.2;
+    this.emmm.play();
+  }
+  next() {
+    let curr = this.womanIterator.next();
+    if (!curr.done) {
+      this.emmm.play();
+      this.currWoman = curr.value;
+      this.render();
+    } else {
+      document.dispatchEvent(new Event("dayEnd"));
     }
-    next() {
-        let curr = this.womanIterator.next();
-        if (!curr.done) {
-            this.emmm.play()
-            this.currWoman = curr.value;
-            this.render();
-        } else {
-            document.dispatchEvent(new Event("dayEnd"));
-        }
-    }
-    render(){
-        let womanDiv = document.getElementById("woman");
-        womanDiv.style.backgroundImage = `url(/assets/${this.currWoman.type}.png)`;
-        womanDiv.style.zIndex = 0;
-    }
+  }
+  render() {
+    let womanDiv = document.getElementById("woman");
+    womanDiv.style.backgroundImage = `url(./assets/${this.currWoman.type}.png)`;
+    womanDiv.style.zIndex = 0;
+  }
 }
-function* iterator (list = womanList) {
-    for (let item of list) {
-        yield item;
-    }
+function* iterator(list = womanList) {
+  for (let item of list) {
+    yield item;
+  }
 }
 const womanList = [
-    {
-        name: 'Susan',
-        type: 'old',
-        dialogue: susanDialogueMap
-    },
-    {
-        name: 'Alice',
-        type: 'young',
-        dialogue: aliceDialogueMap
-    },
-    {
-        name: 'Caroline',
-        type: 'beauty',
-        dialogue: carolineDialogueMap
-    },
-]
+  {
+    name: "Susan",
+    type: "old",
+    dialogue: susanDialogueMap,
+  },
+  {
+    name: "Alice",
+    type: "young",
+    dialogue: aliceDialogueMap,
+  },
+  {
+    name: "Caroline",
+    type: "beauty",
+    dialogue: carolineDialogueMap,
+  },
+];
--- animationControl.js
+++ animationControl.js
@@ -1,21 +1,20 @@
 export let womanAnimationStatus = {
-    lying: 'url(/assets/guestCome_no_repeat.gif)',
-    leave: 'url(/assets/guestLeave_no_repeat.gif)',
-}
+  lying: "url(./assets/guestCome_no_repeat.gif)",
+  leave: "url(./assets/guestLeave_no_repeat.gif)",
+};
 export let playerAnimationStatus = {
-    stop: 'url(/assets/player.png)',
-    animation: 'url(/assets/player.gif)'
-}
-
+  stop: "url(./assets/player.png)",
+  animation: "url(./assets/player.gif)",
+};
 
 export let setWomanAnimation = (movement) => {
-    document.getElementById("guest").style.backgroundImage = `${movement}`
-}
+  document.getElementById("guest").style.backgroundImage = `${movement}`;
+};
 
 export let setPlayerAnimation = (status) => {
-    document.getElementById("player").style.backgroundImage = status;
-}
+  document.getElementById("player").style.backgroundImage = status;
+};
 
-export  let switchBrightness = (value) => {
-    document.getElementById('game').style.filter = `brightness(${value})`;
-}
+export let switchBrightness = (value) => {
+  document.getElementById("game").style.filter = `brightness(${value})`;
+};
